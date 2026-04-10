6a4217e1150f0f7b7679b1af8a5ae70c10a1d77a
DrDimon <sslinneberg@gmail.com>
2026-04-10 23:08:29 +0200

    Add first drawing

---
+++ drawing.gd
@@ -0,0 +1,18 @@
+class_name Drawing
+extends Node2D
+
+# Should be between 1 and 10
+var greyness_level: int = 1
+
+func _ready():
+	update_color()
+
+func increase_interaction():
+	if(greyness_level == 10):
+		return
+	greyness_level = greyness_level + 1
+	update_color()
+
+func update_color():
+	var color_val = 0.1*float(greyness_level)
+	$Sprite2D.modulate = Color(color_val, color_val, color_val)
