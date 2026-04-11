b50e7cb4f6a33eed93695bdc4ee1197fcf626b9b
Niels G. W. Serup <ngws@metanohi.name>
2026-04-11 13:17:34 +0200

    Fix comment

--- prompt_drop_point.gd
+++ prompt_drop_point.gd
@@ -6,5 +6,5 @@ func _can_drop_data(_at_position: Vector2, data: Variant) -> bool:
 	return true
 
 func _drop_data(_at_position: Vector2, data: Variant) -> void:
-	# Assume that the first child of the control is the drawing.
+	# The parent of the control element is always the drawing.
 	prompt.add_drawing(data.get_parent())
