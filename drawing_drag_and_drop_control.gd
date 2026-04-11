5ed689ee296a07ada12ce42fb667086ae7aa27ec
Niels G. W. Serup <ngws@metanohi.name>
2026-04-11 13:18:46 +0200

    Add comment and fix naming

--- drawing_drag_and_drop_control.gd
+++ drawing_drag_and_drop_control.gd
@@ -1,12 +1,13 @@
 extends Control
 
 func _get_drag_data(position):
-	var drawing := get_parent().get_child(0).duplicate()
+	# The sprite is always the first child.
+	var sprite2d := get_parent().get_child(0).duplicate()
 	var scale = 0.6
-	drawing.scale = Vector2(scale, scale)
-	drawing.position = Vector2(0, 0)
-	drawing.rotation = 0.1
+	sprite2d.scale = Vector2(scale, scale)
+	sprite2d.position = Vector2(0, 0)
+	sprite2d.rotation = 0.1
 	var preview := Control.new()
-	preview.add_child(drawing)
+	preview.add_child(sprite2d)
 	set_drag_preview(preview)
 	return self
