c6d3fcedcf59f8b05c07a9831348a95e5ef8dea7
Niels G. W. Serup <ngws@metanohi.name>
2026-04-11 11:26:23 +0200

    Support dragging (no drop point yet)
    Currently hardcoded to the single drawing.

---
+++ drawing_drag_and_drop_control.gd
@@ -0,0 +1,13 @@
+extends Control
+
+func _get_drag_data(position):
+	var drawing := $Drawing.duplicate()
+	var a = $Drawing.transform
+	var scale = 0.6
+	drawing.scale = Vector2(scale, scale)
+	drawing.position = Vector2(0, 0)
+	drawing.rotation = 0.1
+	var preview := Control.new()
+	preview.add_child(drawing)
+	set_drag_preview(preview)
+	return self
