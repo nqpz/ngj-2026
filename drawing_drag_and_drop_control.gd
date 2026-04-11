d3eb71187f1aab13bfc1400cf5f4efccf932ed83
Niels G. W. Serup <ngws@metanohi.name>
2026-04-11 16:45:57 +0200

    Don't shadow existing scale variable

--- drawing_drag_and_drop_control.gd
+++ drawing_drag_and_drop_control.gd
@@ -3,8 +3,8 @@ extends Control
 func _get_drag_data(position):
 	# The sprite is always the first child.
 	var sprite2d := get_parent().get_child(0).duplicate()
-	var scale := 0.6
-	sprite2d.scale = Vector2(scale, scale)
+	var s := 0.6
+	sprite2d.scale = Vector2(s, s)
 	sprite2d.position = Vector2(0, 0)
 	sprite2d.rotation = 0.1
 	var preview := Control.new()
